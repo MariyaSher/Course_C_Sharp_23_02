0ceaba715852547bf7e396387d4123644cbdbcef
Mariya_Sher <shmv_86@mail.ru>
2023-02-26 18:57:27 +0300

    1_2_HW

--- lesson_1/1_1_HW/Program.cs
+++ lesson_1/1_1_HW/Program.cs
@@ -6,7 +6,7 @@ int b = 7;
 int max = a;
 
 if (a > max) max = a;
-if (b >max) max = b;
+if (b > max) max = b;
 
 Console.Write ("max =");
 Console.WriteLine (max);
---
+++ lesson_1/1_2_HW/Program.cs
@@ -0,0 +1,17 @@
+﻿// 2. Напишите программу, которая принимает на вход три числаи выдает 
+//    максимальное из них.
+
+Console.WriteLine("Нахождение наибольшего из трех чисел");
+
+int a = 2;
+int b = 3;
+int c = 7;
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+
+Console.Write( "max =");
+Console.WriteLine (max);
