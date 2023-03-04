1a5ca5163681c185f872ff30f5ee43fcf42a78e6
Mariya_Sher <shmv_86@mail.ru>
2023-03-04 23:48:07 +0300

    3_2_HW

--- lesson_1/lesson_3/HW_3_2/Program.cs
+++ lesson_1/lesson_3/HW_3_2/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 2. Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними
+// в 3D пространстве
+// AB = √(xb - xa)2 + (yb - ya)2 + (zb - za)2
+
+double dis(double x1,double y1, double z1, double x2, double y2, double z2)
+{
+    return Math.Sqrt(Math.Pow(x2-x1,2)+Math.Pow(y2-y1,2)+Math.Pow(z2-z1,2));
+
+}
+Console.WriteLine(dis(7,-5,0,1,-1,9));
