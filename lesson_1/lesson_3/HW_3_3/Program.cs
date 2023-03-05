b7163bcfd1e7a3128aff5b0d0113f83fc4fa2db6
Mariya_Sher <shmv_86@mail.ru>
2023-03-05 14:48:36 +0300

    3_3_HW

---
+++ lesson_1/lesson_3/HW_3_3/Program.cs
@@ -0,0 +1,16 @@
+﻿// 3. Напишите программу, которая принимаетна вход число (N) и выдает таблицу кубов чисел от 1 до N.
+
+int num = int.Parse(Console.ReadLine()!);
+
+void Div(int n)
+{
+    int i = 1;
+
+    while(n>=i)
+    {
+        Console.WriteLine(Math.Pow(i,3));
+        i++;
+
+    }
+}
+Div(num);
