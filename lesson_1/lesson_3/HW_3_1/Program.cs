87ed9c2c729e39f175a3492fba50278aecf37d51
Mariya_Sher <shmv_86@mail.ru>
2023-03-05 15:25:28 +0300

    3_1_HW

--- lesson_1/lesson_3/HW_3_1/Program.cs
+++ lesson_1/lesson_3/HW_3_1/Program.cs
@@ -1,1 +1,28 @@
 ﻿// 1.напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+void CheckPalindromicNumber(int number)
+{
+    if(number >= 10000)
+    {
+        int num1 = number / 10000;
+        int num2 = number % 10;
+ 
+            if(num1 == num2)
+            {
+                number = number / 10;
+                int num3 = (number / 100)% 10;
+                int num4 = number % 10;
+                if(num3== num4)
+                    Console.WriteLine("Да");
+            }
+            else 
+            Console.WriteLine("Нет");
+            
+    }
+    else
+    Console.WriteLine("Некорректное число!");
+}
+ 
+Console.WriteLine("Введите пятизначное число:");
+int number = int.Parse(Console.ReadLine()!);
+CheckPalindromicNumber(number);
