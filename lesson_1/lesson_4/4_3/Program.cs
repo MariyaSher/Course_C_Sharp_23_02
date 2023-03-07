b9df1ff7f9b29edbb1cd028432df8715f91fe855
Mariya_Sher <shmv_86@mail.ru>
2023-03-07 11:33:57 +0300

    Task_4_3

---
+++ lesson_1/lesson_4/4_3/Program.cs
@@ -0,0 +1,14 @@
+﻿// 3. Напишите программу, которая принимает на вход число N  и выдает произведение чесел от 1 дл N. 
+
+int CompositionNum(int num)
+{
+    int mutiply = 1;
+    for (int i = 1; num >=i; i++)
+        mutiply *=i;
+
+        return mutiply;
+}
+
+int A = int.Parse(Console.ReadLine()!);
+int result = CompositionNum(A);
+Console.WriteLine (result);
