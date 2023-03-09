0191a13dcddf068010d1c659e38a4c43c6c3cda6
Mariya_Sher <shmv_86@mail.ru>
2023-03-09 10:47:51 +0300

    HW_4_1

---
+++ lesson_1/lesson_4/lesson_4/HW_4_1/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите цикл, который принимает на вход два числа (А и В)
+// и возводит число А в натуральную степень В.
+
+double PowNum(int a, int b)
+{
+    double n_pow = 1;
+    int b_abs = Math.Abs(b);
+
+    for (int i = 1; i <= b_abs; i++)
+    {
+        if (b > 0)
+            n_pow*=a;
+            else
+                n_pow /=a;
+    }
+    return n_pow;
+}
+
+Console.WriteLine("Введите число: ");
+int num1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Ввести степень числа: ");
+int num2 = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(PowNum(num1, num2));
