6866f12db3b9c82a5fdfa6d252f51bacc8a5a390
Mariya_Sher <shmv_86@mail.ru>
2023-03-09 11:08:59 +0300

    HW_4_2

---
+++ lesson_1/lesson_4/HW_4_2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход число и выдает сумму цифр в числе.
+
+int SumNums_1(int num)
+{
+    int n_sum = 0;
+
+    while (num > 0)
+    {
+        n_sum += num % 10;
+        num /=10;
+    }
+    return n_sum;
+}
+
+int num1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine(SumNums_1(num1));
+
