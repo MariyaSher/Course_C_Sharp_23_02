9586e07d06edd8ceb6b359b70576106d6a5f64a7
Mariya_Sher <shmv_86@mail.ru>
2023-03-10 17:51:50 +0300

    5_1_task

---
+++ lesson_1/lesson_5/5_1/Program.cs
@@ -0,0 +1,37 @@
+﻿// 5.1 Напишите программу замена элементов массива:положительные элементы на соответствующие отрицательные, и наоборот. 
+// [-4, -8, 8, 2] -> [4, 8, -8,-2]
+
+int[] InputRandomMassive(int lengthMas, int minRangeMas, int maxRangeMas)
+{
+    int[] masRandom = new int[lengthMas]; //задаем массив
+    for (int i = 0; i < lengthMas; i++)
+{
+    masRandom[i] = new Random().Next(minRangeMas, maxRangeMas); //случайные числа в диапазоне
+}
+return masRandom;
+}
+//ввод массива
+void PrintMassive(int[] mas)
+{
+      for (int i = 0; i< mas.Length; i++)
+         Console.Write($" {mas[i]} ");
+      Console.WriteLine();
+}
+
+void MassConverter(int[] arr)
+{
+    for (int i=0; i< arr.Length; i++)
+        arr[i]= -arr[i];
+}
+
+Console.WriteLine("Введите длину массива: ");
+int lengthMas = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите минимальное значение для диапазона случайного числа:  ");
+int minRangeMas = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите максимальное значение для диапазона случайного числа:  "); 
+int maxRangeMas = int.Parse(Console.ReadLine()!);
+
+int[] masRandom = InputRandomMassive(lengthMas, minRangeMas, maxRangeMas);
+PrintMassive(masRandom);
+MassConverter(masRandom);
+PrintMassive(masRandom);
